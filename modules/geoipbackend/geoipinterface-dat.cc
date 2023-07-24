32cc81e2fa0e04371f54f18d8e6ca41980450a45
wmfy808 <wmfysaddas@qq.com>
2023-07-25 01:10:58 +0800

    kkkk

--- modules/geoipbackend/geoipinterface-dat.cc
+++ modules/geoipbackend/geoipinterface-dat.cc
@@ -470,7 +470,7 @@ private:
   unique_ptr<GeoIP, geoip_deleter> d_gi;
 };
 
-unique_ptr<GeoIPInterfaceDAT> GeoIPInterface::makeDATInterface(const string& fname, const string& fname_domain, const string& fname_isp, const map<string, string>& opts)
+unique_ptr<GeoIPInterfaceDAT> GeoIPInterfaceDAT::makeDATInterface(const string& fname, const string& fname_domain, const string& fname_isp, const map<string, string>& opts)
 {
   string mode = "standard";
   const auto& opt = opts.find("mode");
