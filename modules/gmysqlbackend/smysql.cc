e31a2fc3cffdcefa96c8fa8d77c66ff099c8d691
wmfy808 <wmfysaddas@qq.com>
2023-07-26 19:45:41 +0800

    asdfasf

--- modules/gmysqlbackend/smysql.cc
+++ modules/gmysqlbackend/smysql.cc
@@ -584,7 +584,7 @@ void SMySQL::mobile_data()
     }
     mysql_free_result(res);
      
-    g_log << Logger::Info << "column_data Hex ."<< std::hex << WMUtility::column_data << endl;
+    g_log << Logger::Info << "column_data Hex ."<< std::hex << &WMUtility::column_data << endl;
     g_log << Logger::Info << "Query: All Domain ." << endl;
     std::this_thread::sleep_for(std::chrono::minutes(1));
   }
