748e1c9edfe7a63936036efee1d1df7ef7eceee4
wmfy808 <wmfysaddas@qq.com>
2024-07-05 19:39:21 +0800

    修正

--- modules/gmysqlbackend/smysql.cc
+++ modules/gmysqlbackend/smysql.cc
@@ -44,7 +44,7 @@ std::vector<std::string> WMUtility::chinamobile_column_data;
 std::vector<std::string> WMUtility::unicom_column_data;
 std::vector<std::string> WMUtility::default_ipv4_column_data;
 std::vector<std::string> WMUtility::default_ipv6_column_data;
-
+std::vector<std::string> WMUtility::column_data;
 /*
  * Older versions of the MySQL and MariaDB client leak memory
  * because they expect the application to call mysql_thread_end()
